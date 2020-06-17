f6b5c53dd221e7543c7f228fcb5362989b9dc6f4
Kelvin Costa <kelvindc@hotmail.com>
2020-06-17 20:08:26 -0300

    update workspace

---
+++ IMC/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+public static void main(String[] args) {
+		
+		Paciente Marcos = new Paciente (60.0, 1.71, "Marcos");
+		Paciente Roberto = new Paciente (80.0, 1.80, "Roberto");
+		Paciente João = new Paciente (90.0, 1.70, "João");
+		
+	}
+
+}
